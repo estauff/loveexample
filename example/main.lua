6f61e782ccbcefbead786fc207b7c00013bdf098
jeprinz <jacobeliasprinz@gmail.com>
2020-04-16 00:35:24 -0400

    Hrmmmm....

--- example/main.lua
+++ example/main.lua
@@ -1,3 +1,5 @@
+grass = love.graphics.newImage("Grass tile.png")
+
 function love.load()
   x = 100
   y = 100
@@ -10,6 +12,7 @@ function love.update(dt)
 end
 
 function love.draw(dt)
+  love.graphics.draw(grass, 100, 200)
   love.graphics.setColor(0, 0, 255)
   love.graphics.circle('fill', x, y, 25)
 end
