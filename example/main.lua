64dd9a183cee714c19391529b8074d0f1fa7db16
jeprinz <jacobeliasprinz@gmail.com>
2020-04-16 23:21:11 -0400

    Game

--- example/main.lua
+++ example/main.lua
@@ -1,34 +1,62 @@
 grass = love.graphics.newImage("Grass tile.png")
 slimer = love.graphics.newImage("Slime.png")
+forest = love.graphics.newImage("nighttimeforest1.jpg")
+forestWidth, forestHeight = forest:getDimensions()
+
+scalex = love.graphics:getWidth()
+scaley = love.graphics:getHeight()
 
 function love.load()
   x = 100
   y = 100
-  accel = 10
-  velocity = 0
+  vx = 0
+  vy = 0
 end
+
+minHeight = scaley - slimer:getHeight()
 
 function love.update(dt)
   if love.keyboard.isDown('right') then
-    x = x + 100*dt
+    vx = 400
   end
   if love.keyboard.isDown('left') then
-    x = x - 100*dt
+    vx = -400
   end
-  if love.keyboard.isDown('up') then
-  y = y - 100*dt
+  if love.keyboard.isDown('up') and y == minHeight then
+    vy = -2000
   end
-  if love.keyboard.isDown('down') then
-    y = y + 100*dt
+
+  if y < minHeight then
+    vy = vy + 100
+  else
+    if vy > 0 then
+      vy = 0
+      y = minHeight
+    end
   end
-  x = x + velocity*dt
+  if x < 0 then
+    vx = 0
+    x = 0
+  end
+  if x > scalex - slimer:getWidth() then
+    vx = 0
+    x = scalex - slimer:getWidth()
+  end
+
+  x = x + vx*dt
+  y = y + vy*dt
+
+  vx = vx * .9
+  vy = vy * .9
 end
 
 function love.draw(dt)
   -- love.graphics.setColor(0, 0, 255)
   -- love.graphics.circle('fill', x, y, 25)
   love.graphics.setColor(255,255,255)
+  love.graphics.draw(forest, 1, 1, 0, scalex / forestWidth, scaley / forestHeight)
+  love.graphics.setColor(0,.2,0)
   love.graphics.draw(slimer, x, y)
-  love.graphics.draw(grass, 100, 200)
+  -- love.graphics.draw(grass, 100, 200)
 
 end
